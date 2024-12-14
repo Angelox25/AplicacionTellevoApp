3e755683fc7e84a502be2798b15520f028e193b3
Angelo Gonzalez <angea.gonzalez@duocuc.cl>
2024-12-14 00:50:05 -0300

    Ultimas modificaciones

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,19 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.TellevoApp.ionic',
-  appName: 'TellevoApp',
-  webDir: 'www'
+  appId: 'com.tellevoapp.app',
+  appName: 'TeLlevoApp',
+  webDir: 'www',
+  bundledWebRuntime: false,
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000, // Duración de la pantalla de inicio (ms)
+      backgroundColor: "#ffffff", // Color de fondo
+      androidScaleType: "CENTER_CROP", // Ajuste de imagen
+      showSpinner: false // Ocultar spinner de carga
+    }
+  }
 };
 
 export default config;
+
